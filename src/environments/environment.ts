dd6bc40e25536638f833cc3ea0e78cf62d9cb3bf
Amine0702 <amineabdallah2k22@gmail.com>
2026-05-24 20:26:16 +0200

    fix vercel build scriptt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  storeName: 'BY DJO',
-  currency: 'TND',
-  currencySymbol: 'د.ت',
-  whatsapp: '+216XXXXXXXX',
-  facebook: 'bydjo',
-  instagram: 'bydjo'
+  apiUrl: "https://bydjobackend-1.onrender.com",
+  storeName: "BY DJO",
+  currency: "TND",
+  currencySymbol: "د.ت",
+  whatsapp: "+216XXXXXXXX",
+  facebook: "bydjo",
+  instagram: "bydjo",
 };
