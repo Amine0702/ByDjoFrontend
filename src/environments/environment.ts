4537866b3ce882cc0c9e2a25fd0410c3dc067124
Amine0702 <amineabdallah2k22@gmail.com>
2026-05-25 13:00:27 +0200

    railway backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://bydjobackend-1.onrender.com/api",
+  apiUrl: "https://bydjobackend-production.up.railway.app/api",
   storeName: "BY DJO",
   currency: "TND",
   currencySymbol: "د.ت",
